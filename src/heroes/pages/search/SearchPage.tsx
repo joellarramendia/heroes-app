17c1c13c5d8d9ebf677bfbf10db7110a2706e1c6
joellarramendia <joellarramendia@gmail.com>
2026-06-11 16:42:04 -0300

    feat: implement HeroStatCard component and integrate it into HeroStats for enhanced character statistics display

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,9 +1,13 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { HeroStats } from "@/heroes/components/HeroStats"
 
 export const SearchPage = () => {
   return (
     <>
       <CustomJumbotron title="Búsqueda de SuperHéroes" description="Descubre, explora y adminstra super héroes y villanos" />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
     </>
   )
 }
