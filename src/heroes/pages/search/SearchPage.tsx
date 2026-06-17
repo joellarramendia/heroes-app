d2b9d74dd5461f0fba9ba88d7bbbbce5dcd1a949
joellarramendia <joellarramendia@gmail.com>
2026-06-17 17:20:06 -0300

    feat: add Accordion and Slider components, enhance SearchControl with advanced filters and hero strength slider

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,25 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControl } from "./ui/SearchControl"
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useQuery } from "@tanstack/react-query"
+import { useSearchParams } from "react-router"
+import { searchHeroAction } from "@/heroes/actions/search-hero.action"
 
 export const SearchPage = () => {
+
+  const [searchParms] = useSearchParams()
+
+  const name = searchParms.get('name') ?? undefined
+  const strength = searchParms.get('strength') ?? undefined
+
+
+  const {data: heroes = []} = useQuery({
+    queryKey: ['search', {name, strength}],
+    queryFn: () => searchHeroAction({name, strength}),
+    staleTime: 1000 * 60 * 5
+  })
+
   return (
     <>
       <CustomJumbotron title="Búsqueda de SuperHéroes" description="Descubre, explora y adminstra super héroes y villanos" />
@@ -15,6 +32,8 @@ export const SearchPage = () => {
 
       {/* Filter and search */}
       <SearchControl />
+
+      <HeroGrid heroes={heroes}/>
     </>
   )
 }
