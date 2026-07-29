cfa24fac0d91e8f4bcda831fd6da1f461d250e89
joellarramendia <joellarramendia@gmail.com>
2026-07-29 20:50:22 -0300

    Add tests for SearchControl component and corresponding snapshots

--- src/heroes/pages/search/SearchPage.test.tsx
+++ src/heroes/pages/search/SearchPage.test.tsx
@@ -13,6 +13,10 @@ vi.mock('@/components/custom/CustomJumbotron', () => ({
     CustomJumbotron: () => <div data-testid="custom-jumbotron"></div>
 }))
 
+vi.mock('./ui/SearchControl', () => ({
+    SearchControl: () => <div data-testid="search-controls"></div>
+}))
+
 vi.mock('@/heroes/components/HeroGrid', () => ({
     HeroGrid: ({heroes}: {heroes: Hero[]}) => (<div data-testid="hero-grid">
         {
---
+++ src/heroes/pages/search/ui/SeachControl.test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
+import { describe, test, expect, vi } from 'vitest'
+import { SearchControl } from './SearchControl'
+
+if (typeof window.ResizeObserver === 'undefined') {
+    class ResizeObserver {
+        observe() { }
+        unobserve() { }
+        disconnect() { }
+    }
+    window.ResizeObserver = ResizeObserver
+}
+
+const renderWithRouter = (initialEntries: string[] = ['/']) => {
+    return render(
+        <MemoryRouter initialEntries={initialEntries}>
+            <SearchControl />
+        </MemoryRouter>
+    )
+}
+
+describe('SearchControl', () => {
+    test('should render SearchControls with default values', () => {
+        const { container } = renderWithRouter()
+
+        expect(container).toMatchSnapshot()
+    })
+
+
+    test('should set input value when search para name is set', () => {
+        renderWithRouter(['/?name=Batman'])
+
+        const input = screen.getByPlaceholderText('Buscar heroes, villanos, poderes, equipos...')
+
+        expect(input.getAttribute('value')).toBe('Batman')
+    })
+
+
+    test('should change params when input is changed and enter is pressed', () => {
+        renderWithRouter(['/?name=Batman'])
+
+        const input = screen.getByPlaceholderText('Buscar heroes, villanos, poderes, equipos...')
+
+        expect(input.getAttribute('value')).toBe('Batman')
+
+        fireEvent.change(input, { target: { value: 'Superman' } })
+        fireEvent.keyDown(input, { key: 'Enter' })
+
+        expect(input.getAttribute('value')).toBe('Superman')
+
+
+    })
+
+})
